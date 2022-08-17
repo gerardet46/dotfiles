81d3550accd98ea63cef4cc437785240c5b7434b
gerardet46 <gerard@ricebes.cat>
2022-08-17 18:35:57 +0200

    deleted old scripts/ folder -> sc/

--- scripts/cron/drive.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-#rsync -a --delete -e ssh root@161.97.95.10:/srv/sftp/* /home/gerry/Drive
-#notify-send "Fet - rsync"
-
-nextcloudcmd -u gerry -p "$(pass banc)" $HOME/Nextcloud https://gerryprovesweb.xyz/
-
-notify-send "Fet - Nextcloud"
--- scripts/cron/mail.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-mailsync
-
-pkill -RTMIN+12 dwmblocks
-
-notify-send "Correu sincronitzat"
