d0bec0323063c8c4cf5c18c16c89a361f86647b6
gerardet46 <gerard@ricebes.cat>
2023-11-24 16:10:42 +0100

    Actualització totxa

--- .config/tg/conf.py
+++ .config/tg/conf.py
@@ -11,8 +11,9 @@ PHONE = get_pass('telefon')
 LONG_MSG_CMD = "nvim + -c 'startinsert' {file_path}"
 
 # notifications
-NOTIFY_CMD = "pidof dwm && notify-send -i {icon_path} {title} {msg}"
-# NOTIFY_CMD = ""
+#NOTIFY_CMD = "pidof dwm && notify-send -i {icon_path} {title} {msg} || echo '{title} {msg}' >$HOME/.config/tmux/status"
+# NOTIFY_CMD = "pidof dwm && notify-send -i {icon_path} {title} {msg}"
+NOTIFY_CMD = ""
 
 # voice recording
 VOICE_RECORD_CMD = (
